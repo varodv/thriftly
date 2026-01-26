77d41d7a33aebf4ef0625671f3b1996cd6ca0036
varodv <varodv@gmail.com>
2026-01-26 19:03:41 +0100

    build(i18n): Integrate i18n (react-inlt)

---
+++ providers/intl-provider.tsx
@@ -0,0 +1,11 @@
+import type { PropsWithChildren } from 'react';
+import { IntlProvider as ReactIntlProvider } from 'react-intl';
+import { MESSAGES } from '@/i18n';
+
+export function IntlProvider({ children }: PropsWithChildren) {
+  return (
+    <ReactIntlProvider locale="en" messages={MESSAGES}>
+      {children}
+    </ReactIntlProvider>
+  );
+}
