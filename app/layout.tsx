9967342863ff429bb6804f2e8ade247750cdf5a2
varodv <varodv@gmail.com>
2026-02-16 19:17:30 +0100

    feat(app): Disable zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { IntlProvider } from '@/providers/intl-provider';
 import { ThemeProvider } from '@/providers/theme-provider';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'thriftly',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
