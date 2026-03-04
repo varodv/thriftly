57ab9ea509d74575f00771c279324ca3b25af784
varodv <varodv@gmail.com>
2026-03-05 00:02:54 +0100

    feat(balance): Improve cash flow chart

--- components/balance-card.tsx
+++ components/balance-card.tsx
@@ -1,14 +1,11 @@
-import type { ChartConfig } from './ui/chart';
 import type { Transaction } from '@/hooks/use-transaction';
-import { isThisMonth } from 'date-fns';
 import { ChevronDownIcon } from 'lucide-react';
 import { useMemo, useState } from 'react';
 import { FormattedNumber, useIntl } from 'react-intl';
-import { Bar, BarChart, CartesianGrid, Rectangle, XAxis } from 'recharts';
 import { useDate } from '@/hooks/use-date';
 import { cn } from '@/lib/utils';
+import { CashFlowChart } from './cash-flow-chart';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
-import { ChartContainer } from './ui/chart';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible';
 
 interface Props {
@@ -16,25 +13,10 @@ interface Props {
   transactions: Array<Transaction>;
 }
 
-interface DataItem {
-  date: Date;
-  income: number;
-  expense: number;
-}
-
 export function BalanceCard({ className, transactions }: Props) {
   const { $t } = useIntl();
 
   const { formatDate } = useDate();
-
-  const config: ChartConfig = {
-    income: {
-      color: 'var(--color-green-500)',
-    },
-    expense: {
-      color: 'var(--color-red-500)',
-    },
-  };
 
   const [open, setOpen] = useState(false);
 
@@ -42,36 +24,6 @@ export function BalanceCard({ className, transactions }: Props) {
     () => transactions.reduce((result, transaction) => result + transaction.amount, 0),
     [transactions],
   );
-
-  const data = useMemo(
-    () =>
-      transactions
-        .reduce<Array<DataItem>>((result, transaction) => {
-          const date = new Date(transaction.timestamp);
-          date.setDate(1);
-          date.setHours(0, 0, 0, 0);
-          let item = result.find(currentItem => currentItem.date.getTime() === date.getTime());
-          if (!item) {
-            item = {
-              date,
-              income: 0,
-              expense: 0,
-            };
-            result.push(item);
-          }
-          if (transaction.amount > 0) {
-            item.income += transaction.amount;
-          }
-          else {
-            item.expense -= transaction.amount;
-          }
-          return result;
-        }, [])
-        .slice(-6),
-    [transactions],
-  );
-
-  const activeIndex = useMemo(() => data.findIndex(item => isThisMonth(item.date)), [data]);
 
   return (
     <Collapsible open={open} onOpenChange={setOpen}>
@@ -106,59 +58,14 @@ export function BalanceCard({ className, transactions }: Props) {
             </CardHeader>
             {!!transactions.length && (
               <ChevronDownIcon
-                className={cn('size-4 transition-transform', { 'rotate-180': open })}
+                className={cn('size-4 m-2 transition-transform', { 'rotate-180': open })}
               />
             )}
           </div>
         </CollapsibleTrigger>
         <CollapsibleContent>
           <CardContent className="p-0">
-            <ChartContainer config={config}>
-              <BarChart accessibilityLayer data={data}>
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="date"
-                  tickLine={false}
-                  tickMargin={10}
-                  axisLine={false}
-                  tickFormatter={(value: Date) => formatDate(value, { month: 'short' })}
-                />
-                <Bar
-                  dataKey="income"
-                  fill="var(--color-income)"
-                  radius={4}
-                  activeIndex={activeIndex}
-                  activeBar={({ ...props }) => {
-                    return (
-                      <Rectangle
-                        {...props}
-                        fillOpacity={0.8}
-                        stroke="var(--color-income)"
-                        strokeDasharray={4}
-                        strokeDashoffset={4}
-                      />
-                    );
-                  }}
-                />
-                <Bar
-                  dataKey="expense"
-                  fill="var(--color-expense)"
-                  radius={4}
-                  activeIndex={activeIndex}
-                  activeBar={({ ...props }) => {
-                    return (
-                      <Rectangle
-                        {...props}
-                        fillOpacity={0.8}
-                        stroke="var(--color-expense)"
-                        strokeDasharray={4}
-                        strokeDashoffset={4}
-                      />
-                    );
-                  }}
-                />
-              </BarChart>
-            </ChartContainer>
+            <CashFlowChart transactions={transactions} maxItems={6} />
           </CardContent>
         </CollapsibleContent>
       </Card>
---
+++ components/cash-flow-chart.tsx
@@ -0,0 +1,161 @@
+import type { ChartConfig } from './ui/chart';
+import type { Transaction } from '@/hooks/use-transaction';
+import { isThisMonth } from 'date-fns';
+import { useMemo } from 'react';
+import { useIntl } from 'react-intl';
+import {
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Label,
+  Rectangle,
+  ReferenceLine,
+  XAxis,
+} from 'recharts';
+import { useDate } from '@/hooks/use-date';
+import { ChartContainer } from './ui/chart';
+
+interface Props {
+  className?: string;
+  transactions: Array<Transaction>;
+  maxItems?: number;
+}
+
+interface DataItem {
+  date: Date;
+  income: number;
+  expense: number;
+}
+
+export function CashFlowChart({ className, transactions, maxItems = 0 }: Props) {
+  const { formatNumber } = useIntl();
+
+  const { formatDate } = useDate();
+
+  const config: ChartConfig = {
+    income: {
+      color: 'var(--color-green-500)',
+    },
+    expense: {
+      color: 'var(--color-red-500)',
+    },
+  };
+
+  const data = useMemo(
+    () =>
+      transactions
+        .reduce<Array<DataItem>>((result, transaction) => {
+          const date = new Date(transaction.timestamp);
+          date.setDate(1);
+          date.setHours(0, 0, 0, 0);
+          let item = result.find(currentItem => currentItem.date.getTime() === date.getTime());
+          if (!item) {
+            item = {
+              date,
+              income: 0,
+              expense: 0,
+            };
+            result.push(item);
+          }
+          if (transaction.amount > 0) {
+            item.income += transaction.amount;
+          }
+          else {
+            item.expense -= transaction.amount;
+          }
+          return result;
+        }, [])
+        .slice(-maxItems),
+    [transactions, maxItems],
+  );
+
+  const meanIncome = useMemo(() => {
+    const { total, count } = data.reduce(
+      (result, item) => {
+        if (!isThisMonth(item.date)) {
+          result.total += item.income;
+          result.count++;
+        }
+        return result;
+      },
+      { total: 0, count: 0 },
+    );
+    return total / count;
+  }, [data]);
+
+  const meanExpense = useMemo(() => {
+    const { total, count } = data.reduce(
+      (result, item) => {
+        if (!isThisMonth(item.date)) {
+          result.total += item.expense;
+          result.count++;
+        }
+        return result;
+      },
+      { total: 0, count: 0 },
+    );
+    return total / count;
+  }, [data]);
+
+  const activeIndex = useMemo(() => data.findIndex(item => isThisMonth(item.date)), [data]);
+
+  return (
+    <ChartContainer className={className} config={config}>
+      <ComposedChart accessibilityLayer data={data} margin={{ top: 8 }}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="date"
+          axisLine={false}
+          tickLine={false}
+          tickFormatter={(value: Date) => formatDate(value, { month: 'short' })}
+        />
+        <Bar
+          dataKey="income"
+          fill="var(--color-income)"
+          radius={4}
+          activeIndex={activeIndex}
+          activeBar={({ ...props }) => {
+            return (
+              <Rectangle
+                {...props}
+                fillOpacity={0.75}
+                stroke="var(--color-income)"
+                strokeDasharray={4}
+              />
+            );
+          }}
+        />
+        <Bar
+          dataKey="expense"
+          fill="var(--color-expense)"
+          radius={4}
+          activeIndex={activeIndex}
+          activeBar={({ ...props }) => {
+            return (
+              <Rectangle
+                {...props}
+                fillOpacity={0.75}
+                stroke="var(--color-expense)"
+                strokeDasharray={4}
+              />
+            );
+          }}
+        />
+        <ReferenceLine y={meanIncome} stroke="var(--color-income)" strokeDasharray={4}>
+          <Label
+            value={formatNumber(meanIncome, { format: 'currency' })}
+            position="insideBottomRight"
+            fill="var(--color-foreground)"
+          />
+        </ReferenceLine>
+        <ReferenceLine y={meanExpense} stroke="var(--color-expense)" strokeDasharray={4}>
+          <Label
+            value={formatNumber(meanExpense, { format: 'currency' })}
+            position="insideBottomRight"
+            fill="var(--color-foreground)"
+          />
+        </ReferenceLine>
+      </ComposedChart>
+    </ChartContainer>
+  );
+}
